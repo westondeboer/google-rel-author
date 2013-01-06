5814db0db8f31c5bccc7c153a1bc85ff21a53fdd
weston deboer <poil11@gmail.com>
2013-01-05 18:40:15 -0800

    fixed order
    fixed order

--- rel-author.php
+++ rel-author.php
@@ -9,16 +9,26 @@ Author URI: http://westondeboer.com
 License: GPLv3
 */
 
+
 function wd_add_google_profile( $contactmethods ) {
-	// Add Google Profiles
-	$contactmethods['google_profile'] = 'Google Profile URL';
-	return $contactmethods;
+	
+$contactmethods['google_profile'] = 'Google Profile URL';
+	
+return $contactmethods;
+
 }
+
 add_filter( 'user_contactmethods', 'wd_add_google_profile', 10, 1);
 
+
 function add_google_rel_author() {
+
 $author_gplus = get_the_author_meta('google_profile');
+
 if ($author_gplus)
+
 echo '<link rel="author" href="https://plus.google.com/' .$author_gplus. '/posts"/>';
+
 }
+
 add_action('wp_head', 'add_google_rel_author');
